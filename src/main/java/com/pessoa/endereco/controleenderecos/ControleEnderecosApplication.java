a0a80c4282ff1ef594cecfc19f5ddab7a1ff3536
lucasfreitas242 <lucasfrittas@gmail.com>
2021-04-24 00:40:16 -0300

    Registro Servlet e configuração do BD H2

--- src/main/java/com/pessoa/endereco/controleenderecos/ControleEnderecosApplication.java
+++ src/main/java/com/pessoa/endereco/controleenderecos/ControleEnderecosApplication.java
@@ -2,9 +2,17 @@ package com.pessoa.endereco.controleenderecos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ControleEnderecosApplication {
+public class ControleEnderecosApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(ControleEnderecosApplication.class);
+	
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleEnderecosApplication.class, args);
