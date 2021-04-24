aae810fae7d4a17a48ca80ec65da72b9cc8993f2
lucasfreitas242 <lucasfrittas@gmail.com>
2021-04-23 23:59:48 -0300

    Criação do Projeto Spring

---
+++ src/main/java/com/pessoa/endereco/controleenderecos/ControleEnderecosApplication.java
@@ -0,0 +1,13 @@
+package com.pessoa.endereco.controleenderecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEnderecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEnderecosApplication.class, args);
+	}
+
+}
